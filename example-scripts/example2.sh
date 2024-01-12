afe9463cb5564523de0acb07a3f54c5a6f177da1
Praveen_AWS_ubuntu <rpraveen4712@gmail.com>
2024-01-12 05:36:05 +0000

    example2 and example3 completed

---
+++ example-scripts/example2.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+myname="Praveen"
+profession="DevOps Engineer"
+
+echo "Hello, My name is $myname."
+echo "And I'm a $profession."
+
+echo 'Hello, My name is $myname.'
+echo 'And I'm a $profession.'
---
+++ example-scripts/example3.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+store=$(ls)
+echo $store
+
+store2=$(ls -l)
+echo $store2
+
+store3=$(pwd)
+echo $store3
