f54828a3f417e7a5ca997348f390310228d880ac
PraveenReddy2510 <127923130+PraveenReddy2510@users.noreply.github.com>
2024-01-12 12:37:39 +0530

    Update example2.sh

--- example-scripts/example2.sh
+++ example-scripts/example2.sh
@@ -7,6 +7,6 @@ echo "Hello, My name is $myname."
 echo "And I'm a $profession."
 
 echo 'Hello, My name is $myname.' 
-# here the string within single quotes will be printed as it is because variable name is not mentioned between double quotes
+# here the string within single quotes will be printed as it is because variable name is not mentioned between double quotes.
 echo 'And I'm a $profession.'
-# here thi throw a error
+# here this throws a error, so to overcome this scenario use " echo 'And I\'m a $profession.'" to run successfully.
