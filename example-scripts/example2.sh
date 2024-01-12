427b05473ed21ad2cd55af12820e1fbb990a0e45
PraveenReddy2510 <127923130+PraveenReddy2510@users.noreply.github.com>
2024-01-12 11:11:36 +0530

    Update example2.sh

--- example-scripts/example2.sh
+++ example-scripts/example2.sh
@@ -6,5 +6,7 @@ profession="DevOps Engineer"
 echo "Hello, My name is $myname."
 echo "And I'm a $profession."
 
-echo 'Hello, My name is $myname.'
+echo 'Hello, My name is $myname.' 
+# here the string within single quotes will be printed as it is because variable name is not mentioned between double quotes
 echo 'And I'm a $profession.'
+# here thi throw a error
